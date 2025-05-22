8e51bcc50c0c0bfefd45452feb247825c0c4b815
Helder <helder@media4all.com.br>
2025-05-21 22:49:59 -0300

    Refactor user controller to standardize service imports and improve code readability

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -2,10 +2,10 @@ import { Request, Response } from "express";
 
 import { UserDTO, PassWordUpdateDTO, UserAuthenticationDTO } from "./user.dto";
 
-import * as UserService from "./user.service";
+import * as Service from "./user.service";
 
 export async function createUser(request: Request, response: Response): Promise<void> {
-	const result = await UserService.createUser({
+	const result = await Service.createUser({
 		responsibleDocument: request.user?.document,
 		...request.body
 	} as UserDTO);
@@ -14,13 +14,13 @@ export async function createUser(request: Request, response: Response): Promise<
 }
 
 export async function authenticateUser(request: Request, response: Response): Promise<void> {
-	const result = await UserService.authenticateUser(request.body as UserAuthenticationDTO);
+	const result = await Service.authenticateUser(request.body as UserAuthenticationDTO);
 
 	response.status(result.statusCode).json(result);
 }
 
 export async function updateUserPassword(request: Request, response: Response): Promise<void> {
-	const result = await UserService.changeUserPassword({
+	const result = await Service.changeUserPassword({
 		document: request.user?.document,
 		...request.body
 	} as PassWordUpdateDTO);
