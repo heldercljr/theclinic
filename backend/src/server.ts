bb5153e67500da6593780e8ce8753b4dce0b0385
Helder <helder.junior@ccc.ufcg.edu.br>
2025-06-04 14:09:29 -0300

    Refactor server.ts to add type annotations for port and host variables

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
 import app from "./app";
 
-const port = Number(process.env.PORT) || 3000;
-const host = process.env.HOST as string || "127.0.0.1";
+const port: number = Number(process.env.PORT) || 3000;
+const host: string = process.env.HOST as string || "127.0.0.1";
 
-app.listen(port, host, () => {
+app.listen(port, host, (): void => {
     console.log(`Server is running on ${host}:${port} since ${new Date().toLocaleString()}`);
 });
